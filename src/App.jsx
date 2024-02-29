f66634b49bedaa87327cee1544d3e2a9f26904bf
Natalie Kalinkina <ms.nataly.r@yandex.ru>
2024-02-29 18:04:59 +0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='app'>
+      <p>Здесь будет тест валидации DatePicker+Formik+Yup</p>
+    </div>
+  )
+}
+
+export default App
